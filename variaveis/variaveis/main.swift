95d6f943ca84cc5bafe39756c76077244c7fb825
jenifer rocha <jeniferdutra2000@icloud.com>
2025-02-18 22:14:20 -0300

    Variáveis

---
+++ variaveis/variaveis/main.swift
@@ -0,0 +1,48 @@
+//
+//  main.swift
+//  variaveis
+//
+//  Created by Jenifer Rocha on 18/02/25.
+//
+
+import Foundation
+
+// 1 -> var
+// 2 -> nome da variavel
+// 3 -> tipagem (opcional)
+// 4 -> valor
+
+var nomeCachorro = "Duke"
+var amigo = "Albert"
+
+print(nomeCachorro)
+print(amigo)
+
+
+// MARK: Tipagem do tipo explicita
+
+var nomeMami: String = "Lea"
+print(nomeMami)
+
+// MARK: Tipagem do tipo implicita
+
+var numero = 32
+
+// MARK: FLOAT = consegue acessar numeros decimais com menos casas - USA MTO POUCO O FLOAT
+
+var numeroAleatorioFloat: Float = 1.7654389756629293767364728
+print(numeroAleatorioFloat)
+
+// MARK: DOUBLE = consegue acessar mais casas decimais
+
+var numeroAleatorioDouble: Double = 1.7654389756629293767364728
+print(numeroAleatorioDouble)
+
+// MARK: BOOL
+
+var choveuHoje: Bool = false
+
+// MARK: CONSTANTS
+
+let totalDesconto: Double = 0.50
+
