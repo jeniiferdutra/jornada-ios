418aa52362a428d4c245549316bcb75599438912
jenifer rocha <jeniferdutra2000@icloud.com>
2025-02-18 23:16:27 -0300

    manipulando variaveis

---
+++ manipulando-variaveis/manipulando-variaveis/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  manipulando-variaveis
+//
+//  Created by Jenifer Rocha on 18/02/25.
+//
+
+import Foundation
+
+var peso: Double = 56.50
+var pesoInt: Int = Int(peso)
+print(pesoInt)
+
+var altura: Int = 160
+var alturaDouble: Double = Double(altura)
+print(alturaDouble)
+
+var age: String = "25 anos"
+var ageInt: Int = Int(age) ?? 0
+print(ageInt)
