cd2890ac9deb7d7e98604d541b81a08b93a8dacc
jenifer rocha <jeniferdutra2000@icloud.com>
2025-02-20 20:43:02 -0300

    Objetos e Classes

---
+++ objeto/objeto/main.swift
@@ -0,0 +1,47 @@
+//
+//  main.swift
+//  objeto
+//
+//  Created by Jenifer Rocha on 20/02/25.
+//
+
+import Foundation
+
+// MARK: Um objeto pode ser uma variavel, funcao, ou estrutura de dados. Em programacao orientada a objetos, a palavra objeto refere-se a uma instancia de uma classe. Contém as propriedades que o definem, que sao as caracteristicas do objeto.
+
+// MARK: Uma classe é uma estrutura que abstrai um conjunto de objetos com caracteristicas similares. A classe é responsável por definir o comportamento de seus objetos - através de métodos - e os estados possíveis destes objetos - através de atributos.
+
+// MARK: Atributos -> caracteristicas de uma classe/objeto
+// MARK: Metodos -> açoes que uma classe pode ter
+
+// MARK: UMA CLASSE É REFERENTE A UM OBJETO
+
+
+class Casa {
+    
+    var quantidadeDeQuartos: Int = 10
+    var temBanheiro: Bool = true
+    var quantidadeDePortas: Int = 25
+    var estaNoCondominio: Bool = true
+    var corDaCasa: String = "Branco"
+    
+    func reformar() {
+        print("A casa esta em reforma")
+    }
+    
+    func abrirJanela() {
+        print("Minha casa esta com a janela aberta")
+    }
+    
+    func acenderLampada() {
+        print("Minha casa esta com as lampadas acesas")
+    }
+    
+}
+
+// Criar objeto
+var minhaCasa: Casa? = Casa() // () -> Instancia, fazer a leitura de toda a classe.
+print(minhaCasa?.quantidadeDePortas ?? "teste")
+
+
+
