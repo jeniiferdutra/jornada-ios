e04705ecd7c1483b8741b0d74424a6193035622b
jenifer rocha <jeniferdutra2000@icloud.com>
2025-02-25 20:13:48 -0300

    IBAction

--- modulo-2/elementos-basicos-curso/elementos-basicos-curso/ViewController.swift
+++ modulo-2/elementos-basicos-curso/elementos-basicos-curso/ViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var bemVindoLabel: UILabel! //acessar programaticamente todas as caracteristicas de uma label
+    @IBOutlet weak var bemVindoLabel: UILabel! //acessar programaticamente todas as caracteristicas de uma label. Ligacoes do tipo Outlet permanecemos acima do DidLoad
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +23,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func tappedLoginButton(_ sender: UIButton) { // Ligacoes do tipo Action permanecemos abaixo do DidLoad
+        print("Botao pressionado")
+        view.backgroundColor = .green
+    }
+    
+    
 }
 
