3da2a6c2fcbbb0ec880fb13d395a939b49b0416c
jenifer rocha <jeniferdutra2000@icloud.com>
2025-03-06 19:49:20 -0300

    Primeira CustomCell

--- modulo-3/table-view/table-view/ViewController.swift
+++ modulo-3/table-view/table-view/ViewController.swift
@@ -14,12 +14,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.configTableView()
     }
     
+    // configuracao da tableView
     func configTableView() {
         tableView.delegate = self // self é a propria ViewController, de modo passando todas as responsabilidades para a propria classe
         tableView.dataSource = self
+        tableView.register(CustomTableViewCell.nib(), forCellReuseIdentifier: CustomTableViewCell.identifier) // estamos falando q a nossa xib é o nosso identificador
     }
 
 
@@ -29,11 +31,13 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // numero de linhas para cada section
-        return 0
+        return 30
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // retorno de uma celula
-        return UITableViewCell() // retornando uma celula vazia
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as? CustomTableViewCell // as? significa que estamos trabalhando com CustomTableViewCell
+        
+        return cell ?? UITableViewCell() // cell é opcional por causa do as?, caso a cell é nil retorna uma instancia vazia
     }
     
     
--- modulo-3/table-view/table-view/table-view-cell/CustomTableViewCell.swift
+++ modulo-3/table-view/table-view/table-view-cell/CustomTableViewCell.swift
@@ -10,12 +10,22 @@ import UIKit
 // MARK: - XIB é utilizada para quando criamos células e componentes apartados, coisas mto especificas criamos a xib em uma facil manutencao apenas usado só para aquele devido elemento/tela/funcionalidade
 
 class CustomTableViewCell: UITableViewCell {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // static -> nao necessita de uma instancia, n pode ter nenhuma depedencia externa
+    static let identifier: String = "CustomTableViewCell" // nome tem que ser igual do arquivo
+    
+    static func nib() -> UINib { // nib -> referencia para indicar qual o arquivo da xib
+        return UINib(nibName: self.identifier, bundle: nil)
+    }
+    
     // MARK: - CICLO DE VIDA
     
     override func awakeFromNib() { // é a msm coisa do viewDidLoad
         super.awakeFromNib()
-        // Initialization code
+        titleLabel.text = "Jenifer"
     }
 
     
