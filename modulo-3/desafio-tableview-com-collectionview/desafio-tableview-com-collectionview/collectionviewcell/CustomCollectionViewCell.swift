6d1c37927f8a271d6225034e8a79b440d95c1669
jenifer rocha <jeniferdutra2000@icloud.com>
2025-03-12 20:31:39 -0300

    Desafio com TableView com CollectionView

---
+++ modulo-3/desafio-tableview-com-collectionview/desafio-tableview-com-collectionview/collectionviewcell/CustomCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomCollectionViewCell.swift
+//  desafio-tableview-com-collectionview
+//
+//  Created by Jenifer Rocha on 12/03/25.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carImageView: UIImageView!
+    static let identifier: String = "CustomCollectionViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: self.identifier, bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        carImageView.contentMode = .scaleAspectFill
+    }
+    
+    func setupCell(nameImage: String) {
+        carImageView.image = UIImage(named: nameImage)
+    }
+}
