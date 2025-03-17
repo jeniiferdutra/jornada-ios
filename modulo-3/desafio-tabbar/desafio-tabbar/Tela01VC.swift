8782fd860d5311241e032a6925650dc18cb5ac98
jenifer rocha <jeniferdutra2000@icloud.com>
2025-03-17 19:50:57 -0300

    Layout TabBar

--- modulo-3/desafio-tabbar/desafio-tabbar/Tela01VC.swift
+++ modulo-3/desafio-tabbar/desafio-tabbar/Tela01VC.swift
@@ -17,11 +17,24 @@ import UIKit
 // Usuário poderá escolher uma foto da galeria ou da camera depois de escrever seu nome ele poderá pressionar sob o adicionar. Quando adicionar, deve aparecer na tableView o nome e a imagem.
 
 class Tela01VC: UIViewController {
-
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var editPictureButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func tappedEditPictureButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tappedAddButton(_ sender: UIButton) {
+    }
+    
 }
 
