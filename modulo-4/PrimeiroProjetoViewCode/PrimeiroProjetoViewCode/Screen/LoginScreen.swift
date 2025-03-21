487060f50be7f674f9050205ed3d393df65f4572
jenifer rocha <jeniferdutra2000@icloud.com>
2025-03-21 01:22:02 -0300

    Criando o primeiro elemento

---
+++ modulo-4/PrimeiroProjetoViewCode/PrimeiroProjetoViewCode/Screen/LoginScreen.swift
@@ -0,0 +1,25 @@
+//
+//  LoginScreen.swift
+//  PrimeiroProjetoViewCode
+//
+//  Created by Jenifer Rocha on 21/03/25.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ modulo-4/PrimeiroProjetoViewCode/PrimeiroProjetoViewCode/VC/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroProjetoViewCode
+//
+//  Created by Jenifer Rocha on 21/03/25.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
