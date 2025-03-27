f0792b006a1a62df938fda6973e71865e97a3416
jenifer rocha <jeniferdutra2000@icloud.com>
2025-03-26 21:26:10 -0300

    chore: configurar projeto inicial para aprendizado de MVVM

---
+++ modulo-5/PrimeiroAppMVVM/PrimeiroAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Jenifer Rocha on 26/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
