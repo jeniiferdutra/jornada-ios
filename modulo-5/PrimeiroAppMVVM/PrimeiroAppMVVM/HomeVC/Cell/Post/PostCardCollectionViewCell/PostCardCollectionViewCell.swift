d1bcbc8e0ea01b387c1fc6402d405d3aa450ef0e
jenifer rocha <jeniferdutra2000@icloud.com>
2025-03-31 20:39:33 -0300

    feat: criar CollectionView e célula para exibição de posts

---
+++ modulo-5/PrimeiroAppMVVM/PrimeiroAppMVVM/HomeVC/Cell/Post/PostCardCollectionViewCell/PostCardCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  PostCardCollectionViewCell.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Jenifer Rocha on 31/03/25.
+//
+
+import UIKit
+
+//MARK: Próximo passo é criar uma Screen
+
+class PostCardCollectionViewCell: UICollectionViewCell {
+    
+    private var screen: PostCardCollectionViewCellScreen = PostCardCollectionViewCellScreen()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configScreen()
+        screen.configProtocolsCollectionView(delegate: self, dataSource: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configScreen() {
+        contentView.addSubview(screen)
+        screen.pin(to: contentView)
+    }
+    
+}
+
+extension PostCardCollectionViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 450)
+    }
+    
+}
