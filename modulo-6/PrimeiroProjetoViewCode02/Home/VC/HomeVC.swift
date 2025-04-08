07503b3c0417c88a47a06879c7cf2e41b3f23e8f
jenifer rocha <jeniferdutra2000@icloud.com>
2025-04-08 20:00:42 -0300

    chore: configurar CocoaPods e instalar SnapKit

---
+++ modulo-6/PrimeiroProjetoViewCode02/Home/VC/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  PrimeiroProjetoViewCode02
+//
+//  Created by Jenifer Rocha on 08/04/25.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+
+}
---
+++ modulo-6/PrimeiroProjetoViewCode02/Login/Screen/LoginScreen.swift
@@ -0,0 +1,120 @@
+//
+//  LoginScreen.swift
+//  PrimeiroProjetoViewCode02
+//
+//  Created by Jenifer Rocha on 07/04/25.
+//
+
+import UIKit
+
+protocol LoginScreenProtocol: AnyObject {
+    func tappedLoginButton()
+}
+
+class LoginScreen: UIView {
+    
+    private weak var delegate: LoginScreenProtocol?
+    
+    public func delegate(delegate: LoginScreenProtocol?) {
+        self.delegate = delegate
+    }
+    
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let email = UITextField()
+        email.translatesAutoresizingMaskIntoConstraints = false
+        email.autocorrectionType = .no
+        email.backgroundColor = .white
+        email.borderStyle = .roundedRect
+        email.keyboardType = .emailAddress
+        email.placeholder = "Digite seu email"
+        email.textColor = .darkGray
+        return email
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let password = UITextField()
+        password.translatesAutoresizingMaskIntoConstraints = false
+        password.autocorrectionType = .no
+        password.backgroundColor = .white
+        password.borderStyle = .roundedRect
+        password.placeholder = "Digite sua senha"
+        password.isSecureTextEntry = true
+        password.textColor = .darkGray
+        return password
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Logar", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        btn.setTitleColor(.white, for: .normal)
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius = 7.5
+        btn.isEnabled = false
+        btn.backgroundColor = .darkGray.withAlphaComponent(0.6)
+        btn.addTarget(self, action: #selector(tappedLoginButton), for: .touchUpInside)
+        return btn
+    }()
+    
+    @objc func tappedLoginButton(_ sender: UIButton) {
+        print("Botao funcionando")
+        delegate?.tappedLoginButton()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBlue
+        addElements()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addElements() {
+        addSubview(loginLabel)
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(loginButton)
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            
+            loginLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 15),
+            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            emailTextField.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 45),
+            emailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            emailTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            emailTextField.heightAnchor.constraint(equalToConstant: 45),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+            passwordTextField.heightAnchor.constraint(equalTo: emailTextField.heightAnchor),
+            
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 50),
+            loginButton.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor),
+            loginButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor),
+            loginButton.heightAnchor.constraint(equalTo: emailTextField.heightAnchor)
+        ])
+    }
+    
+    public func configTextFieldsDelegate(delegate: UITextFieldDelegate) {
+        emailTextField.delegate = delegate
+        passwordTextField.delegate = delegate
+    }
+
+}
