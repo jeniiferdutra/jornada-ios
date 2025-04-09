f656ebe212134a78ac8e0abb48248184f9cdea64
jenifer rocha <jeniferdutra2000@icloud.com>
2025-04-08 23:14:55 -0300

    feat: adicionar tela de login com autenticação via Firebase

--- modulo-6/PrimeiroAppMVVM/PrimeiroAppMVVM/SceneDelegate.swift
+++ modulo-6/PrimeiroAppMVVM/PrimeiroAppMVVM/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = HomeVC()
+        let vc = LoginVC()
         //let navVC = UINavigationController(rootViewController: vc)
         window.rootViewController = vc
         window.makeKeyAndVisible()
