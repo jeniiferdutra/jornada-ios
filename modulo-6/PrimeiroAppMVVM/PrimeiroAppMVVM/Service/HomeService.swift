c84a337baf09dc7f356a747ec37aa9bc1db61020
jenifer rocha <jeniferdutra2000@icloud.com>
2025-04-10 23:33:47 -0300

    test: criar mock para simular dados na tela de Home

---
+++ modulo-6/PrimeiroAppMVVM/PrimeiroAppMVVM/Service/HomeService.swift
@@ -0,0 +1,27 @@
+//
+//  HomeService.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Jenifer Rocha on 10/04/25.
+//
+
+import UIKit
+
+// MARK: Camada de serviço responsável por fornecer os dados da Home. Atualmente utiliza dados mockados, mas pode ser adaptada para chamadas reais de API futuramente.
+
+class HomeService {
+    
+    // MARK: Ideal para quando se trabalha com chamadas de rede (ex: APIs), onde a resposta pode não ser imediata. Deste modo retornamos atraves de um loading
+    // MARK: O parâmetro `completion` é utilizado para retornar os dados de forma assíncrona quando estiverem prontos.
+
+    func getHomeJson(completion: @escaping (Posts?, Error?) -> Void) {
+        if let url = Bundle.main.url(forResource: "HomeData", withExtension: "json") { // buscar o arquivo HomeData
+            do {
+                let data = try Data(contentsOf: url) // atraves da url esta convertendo o json em um binario
+            } catch  {
+                
+            }
+        }
+    }
+    
+}
