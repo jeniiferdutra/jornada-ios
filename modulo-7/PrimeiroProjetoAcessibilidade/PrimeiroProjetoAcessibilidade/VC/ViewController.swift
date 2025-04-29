2d6da20144d486f3193452b8a1fad3257b635749
jenifer rocha <jeniferdutra2000@icloud.com>
2025-04-28 23:03:56 -0300

    feat: implementa ação de clique do botão

--- modulo-7/PrimeiroProjetoAcessibilidade/PrimeiroProjetoAcessibilidade/VC/ViewController.swift
+++ modulo-7/PrimeiroProjetoAcessibilidade/PrimeiroProjetoAcessibilidade/VC/ViewController.swift
@@ -31,13 +31,44 @@ class ViewController: UIViewController {
         screen?.positionThreeLabel.isAccessibilityElement = true
         screen?.positionFourLabel.isAccessibilityElement = true
         screen?.positionFiveLabel.isAccessibilityElement = true
+        screen?.loginButton.isAccessibilityElement = true
+        screen?.titleLabel.isAccessibilityElement = true
+        
+        // accessibilityLabel -> É o que o VoiceOver irá falar quando o cursor estiver no elemento
+        
+        screen?.positionOneLabel.accessibilityLabel = "Posiçao um"
+        screen?.positionTwoLabel.accessibilityLabel = "Posiçao dois"
+        screen?.positionThreeLabel.accessibilityLabel = "Posiçao tres"
+        screen?.positionFourLabel.accessibilityLabel = "Posiçao quatro"
+        screen?.positionFiveLabel.accessibilityLabel = "Posiçao cinco"
+        
+        screen?.positionOneLabel.accessibilityTraits = .staticText
+        screen?.positionTwoLabel.accessibilityTraits = .staticText
+        screen?.positionThreeLabel.accessibilityTraits = .staticText
+        screen?.positionFourLabel.accessibilityTraits = .staticText
+        screen?.positionFiveLabel.accessibilityTraits = .staticText
+        
+        screen?.loginButton.accessibilityTraits = .button
+        screen?.titleLabel.accessibilityTraits = .header
+        
+        guard let primeiro = screen?.positionOneLabel,
+              let segundo = screen?.positionTwoLabel,
+              let terceiro = screen?.positionThreeLabel,
+              let quarto = screen?.positionFourLabel,
+              let quinto = screen?.positionFiveLabel,
+              let button = screen?.loginButton,
+              let title = screen?.titleLabel else { return }
+        
+        // accessibilityElements -> ele ordena os elementos acessiveis
+        
+        view.accessibilityElements = [primeiro, segundo, terceiro, quarto, quinto, button, title]
     }
 
 }
 
 extension ViewController: ViewControllerScreenProtocol {
     func tappedLoginButton() {
-        print(#function)
+        UIAccessibility.post(notification: .announcement, argument: "Teste o botao foi pressionado")
     }
     
     
