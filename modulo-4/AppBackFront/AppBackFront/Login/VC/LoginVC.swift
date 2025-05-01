6f3f8d28d2376c17d74db028c99d29399f6ba5a6
jenifer rocha <jeniferdutra2000@icloud.com>
2025-04-30 23:37:10 -0300

    feat: implementa autenticação de login com Firebase

--- modulo-4/AppBackFront/AppBackFront/Login/VC/LoginVC.swift
+++ modulo-4/AppBackFront/AppBackFront/Login/VC/LoginVC.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginVC: UIViewController {
     
     var screen: LoginScreen?
+    var auth: Auth?
     
     override func loadView() {
         screen = LoginScreen()
@@ -19,20 +21,80 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         dismissKeyBoard()
+        auth = Auth.auth()
         screen?.delegate(delegate: self)
         screen?.configTextFieldsDelegate(delegate: self)
+        isEnableButton(false)
+    }
+    
+    func validateTextFields() {
+        if (screen?.emailTextField.text ?? "").isValid(validType: .email) && (screen?.passwordTextField.text ?? "").isValid(validType: .password) {
+            isEnableButton(true)
+        } else {
+            isEnableButton(false)
+        }
+    }
+    
+    func isEnableButton(_ isEnable: Bool) {
+        if isEnable {
+            screen?.loginButton.setTitleColor(.white, for: .normal)
+            screen?.loginButton.isEnabled = true
+            screen?.subImageView.alpha = 1
+        } else {
+            screen?.loginButton.setTitleColor(.lightGray, for: .normal)
+            screen?.loginButton.isEnabled = false
+            screen?.subLoginImageView.alpha = 0.4
+        }
     }
     
 }
 
 extension LoginVC: LoginScreenProtocol {
     func tappedLoginButton() {
-        print(#function)
+        auth?.signIn(withEmail: screen?.emailTextField.text ?? "", password: screen?.passwordTextField.text ?? "", completion: { user, error in
+            if error != nil {
+                // deu erro
+                print(error?.localizedDescription ?? "")
+            } else {
+                // sucesso
+                print("sucesso!")
+            }
+        })
     }
     
 }
 
 extension LoginVC: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if textField.text?.isEmpty ?? false {
+            textField.layer.borderWidth = 1.5
+            textField.layer.borderColor = UIColor.red.cgColor
+        } else {
+            switch textField {
+            case self.screen?.emailTextField:
+                if (screen?.emailTextField.text ?? "").isValid(validType: .email) {
+                    screen?.emailTextField.layer.borderWidth = 1
+                    screen?.emailTextField.layer.borderColor = UIColor.white.cgColor
+                } else {
+                    screen?.emailTextField.layer.borderWidth = 1.5
+                    screen?.emailTextField.layer.borderColor = UIColor.red.cgColor
+                }
+            case self.screen?.passwordTextField:
+                if (screen?.passwordTextField.text ?? "").isValid(validType: .password) {
+                    screen?.passwordTextField.layer.borderWidth = 1
+                    screen?.passwordTextField.layer.borderColor = UIColor.white.cgColor
+                } else {
+                    screen?.passwordTextField.layer.borderWidth = 1.5
+                    screen?.passwordTextField.layer.borderColor = UIColor.red.cgColor
+                }
+            default:
+                break
+            }
+        }
+        validateTextFields()
+    }
      
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
