a4ff346b26f4e1986cdde272c520fb6200a2f807
jenifer rocha <jeniferdutra2000@icloud.com>
2025-05-01 02:18:50 -0300

    feat: cria a tela de HomeScreen com estrutura inicial de interface

---
+++ modulo-4/AppBackFront/AppBackFront/Feature/Home/HomeScreen.swift
@@ -0,0 +1,74 @@
+//
+//  HomeScreen.swift
+//  AppBackFront
+//
+//  Created by Jenifer Rocha on 01/05/25.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+    
+    lazy var viewBackground: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        return view
+    }()
+    
+    lazy var logoImageView: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.image = UIImage(named: "vector")
+        return img
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let search = UISearchBar()
+        search.translatesAutoresizingMaskIntoConstraints = false
+        search.clipsToBounds = true
+        search.layer.cornerRadius = 20
+        search.placeholder = "Pesquise pelo nome:"
+        return search
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        // TO DO: Register
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
+        layout.scrollDirection = .horizontal
+        collectionView.setCollectionViewLayout(layout, animated: false)
+        return collectionView
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        // TO DO: Register
+        tableView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addViews()
+    }
+    
+    func addViews() {
+        addSubview(viewBackground)
+        viewBackground.addSubview(logoImageView)
+        viewBackground.addSubview(searchBar)
+        viewBackground.addSubview(collectionView)
+        addSubview(tableView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
