586468b705d3682973487780b1c21b7a84f2229f
jenifer rocha <jeniferdutra2000@icloud.com>
2025-05-01 16:22:45 -0300

    feat: cria mock para simular dados na HomeScreen

---
+++ modulo-4/AppBackFront/AppBackFront/Feature/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  AppBackFront
+//
+//  Created by Jenifer Rocha on 01/05/25.
+//
+
+import UIKit
+
+class HomeViewModel {
+    
+}
---
+++ modulo-4/AppBackFront/AppBackFront/Service/HomeService/HomeService.swift
@@ -0,0 +1,32 @@
+//
+//  HomeService.swift
+//  AppBackFront
+//
+//  Created by Jenifer Rocha on 01/05/25.
+//
+
+import UIKit
+
+protocol HomeServiceDelegate: GenericService {
+     func getHomeFromJson(completion: @escaping completion<NFTData?>)
+    
+}
+
+class HomeService: HomeServiceDelegate {
+    
+    func getHomeFromJson(completion: @escaping completion<NFTData?>) {
+        if let url = Bundle.main.url(forResource: "HomeData", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let nftData: NFTData = try JSONDecoder().decode(NFTData.self, from: data)
+                completion(nftData, nil)
+            } catch {
+                completion(nil, Error.fileDecodingFailed(name: "HomeData", error))
+            }
+        } else {
+            completion(nil, Error.fileNotFound(name: "HomeData"))
+        }
+    }
+    
+
+}
